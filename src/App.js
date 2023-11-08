c6401191414f97b8658d4bfae74a51be602a8375
Mayank039 <114925849+Mayank039@users.noreply.github.com>
2023-11-08 12:27:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { LoginSingup } from './components/LoginSingUp/LoginSingup';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSingup/>
+    </div>
+  );
+}
+
+export default App;
